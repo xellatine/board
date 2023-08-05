835541e6d0cbda02f8b7e64e526e18f5d9e83480
xellatine <no1stark@charzin.com>
2023-08-05 17:39:50 +0900

    #1 QueryDSL 디펜던시 추가

---
+++ src/test/java/com/example/board/controller/DataRestTest.java
@@ -0,0 +1,57 @@
+package com.example.board.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Disabled
+@DisplayName("Data Rest API 테스트")
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DataRestTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @DisplayName("게시글 리스트 조회")
+    @Test
+    void retrievePosts() throws Exception {
+        // given
+
+        // when
+        mockMvc.perform(get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+
+        // then
+
+    }
+
+    @DisplayName("게시글 단건 조회")
+    @Test
+    void retrievePost() throws Exception {
+        // given
+
+        // when
+        mockMvc.perform(get("/api/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+
+        // then
+
+    }
+}
